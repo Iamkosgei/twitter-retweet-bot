13a5a8818e709a733692d04fbc0d1a299f37488f
Iamkosgei <kosgeik2930@gmail.com>
2020-03-18 13:06:49 +0300

    initial commit

--- README.md
+++ README.md
@@ -3,14 +3,14 @@
 A twitter bot which retweets any tweet with the following keywords "flutter app", "flutterapp", "flutterdev", "androiddev", "codenewbies" and "nodejs"
 
 ## Tech
- 1. Python
- 2. Tweepy
+
+1.  Python
+2.  Tweepy
 
 ## Steps
-	1. clone repo
+
+    	1. clone repo
      	2. pip install virtualenv
      	3. python3 -m venv env
      	4. source env/bin/activate
      	5. pip install -r requirements.txt
-
-
---
+++ bot.py
@@ -0,0 +1,49 @@
+import tweepy
+import os
+
+
+# auth
+api_key = os.getenv("api_key")
+api_secret_key = os.getenv("api_secret_key")
+access_token = os.getenv("access_token")
+token_secret = os.getenv("token_secret")
+
+auth = tweepy.OAuthHandler(api_key, api_secret_key)
+auth.set_access_token(access_token, token_secret)
+# auth
+
+os.getenv("api_key")
+os.getenv("api_secret_key")
+os.getenv("access_token")
+os.getenv("token_secret")
+
+
+# init api
+api = tweepy.API(auth)
+# init api
+
+
+class MyStreamListener(tweepy.StreamListener):
+    def on_status(self, tweet):
+        if "RT" not in tweet.text:
+            # print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+            # print(tweet.text)
+            # print(tweet)
+            # print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+
+            if not tweet.retweeted:
+                try:
+                    print("retweeting " + str(tweet.id))
+                    api.retweet(tweet.id)
+                except Exception as e:
+                    print("error " + str(e))
+
+    def on_error(self, status_code):
+        print(status_code)
+
+
+myStreamListener = MyStreamListener()
+myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
+
+myStream.filter(track=["flutter app", "flutterapp",
+                       "flutterdev", "androiddev", "codenewbies", "nodejs"])
